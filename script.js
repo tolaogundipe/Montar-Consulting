d5ab54b5b9c43a74a71b43e4b54cd1d4474ba798
tolaogundipe <ogundipeadetola@gmail.com>
2021-05-08 20:50:15 +0100

    i added a slide out mobile menu and adjusted the header section to look right in various mobile devices apart from the  ipad pro

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function myFunction() {
-    var x = document.getElementById("nav_group");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+// function myFunction() {
+//     var x = document.getElementById("nav_group");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
+
+//for slide out navigation on mobile
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("nav_group").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("nav_group").style.width = "0";
+}
