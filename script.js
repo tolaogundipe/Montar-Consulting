035de2c7ac264c4e149af5330c85c9a43ae0c4e4
tolaogundipe <ogundipeadetola@gmail.com>
2021-05-08 12:17:13 +0100

    all pages are done but bugs need fixing. Bugs involve making the pages look good on various mobile devices

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-var myNav = document.getElementById('nav-bar');
-window.onscroll = function () { 
-    "use strict";
-    if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100 ) {
-        myNav.classList.add("nav-colored");
-        myNav.classList.remove("nav-transparent");
-    } 
-    else {
-        myNav.classList.add("nav-transparent");
-        myNav.classList.remove("nav-colored");
+function myFunction() {
+    var x = document.getElementById("nav_group");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
     }
-};
+  }
